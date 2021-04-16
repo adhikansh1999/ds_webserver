41d9a88f0b2697b655ec5b396e10930c2df19784
adhikansh1999 <singhadhikansh113@gmail.com>
2021-04-16 21:08:51 +0530

    database apis added

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, g, session
+from requester_module import send_request
 from datetime import datetime
 import pymysql
 import os
 
 
 app = Flask(__name__)
+
+# format
+
+# response = send_request(msg)
 
 
 app.secret_key=os.urandom(23564)
@@ -44,17 +49,23 @@ def signup():
 
 		if(acc_type == "buyer"):
 			if username:
-				query = "SELECT password FROM buyers WHERE username=%s"
-				cur = cursor.execute(query, (username))
-				if cur:
+
+				# query = "SELECT password FROM buyers WHERE username=%s"
+				# cur = cursor.execute(query, (username))
+				query = {"type":"read", "method":"login_buyer", "username":username}
+				cur = send_request(query)
+
+				if cur['password'] != None:
 					flash('username already exists')
 					return redirect(url_for('signup'))
 				else:	
 					try:
-						query="INSERT INTO buyers (name, username, emailid, password) VALUES (%s, %s, %s, %s)"
-						cursor.execute(query,(name, username, emailid, password))
-						connection.commit() 
-					
+						# query="INSERT INTO buyers (name, username, emailid, password) VALUES (%s, %s, %s, %s)"
+						# cursor.execute(query,(name, username, emailid, password))
+						# connection.commit() 
+						query = {"type":"write", "method":"signup_buyer", "emailid":emailid, "password":password, "name":name, "username":username}
+						buyer_signup_ack = send_request(query)
+						print(buyer_signup_ack)
 					except:
 						return render_template('alert.html')
 
@@ -62,17 +73,23 @@ def signup():
 					return redirect(url_for('login'))
 		elif(acc_type == "seller"):
 			if username:
-				query = "SELECT password FROM sellers WHERE username=%s"
-				cur = cursor.execute(query, (username))
-				if cur:
+				# query = "SELECT password FROM sellers WHERE username=%s"
+				# cur = cursor.execute(query, (username))
+				query = {"type":"read", "method":"login_seller", "username":username}
+				cur = send_request(query)
+				if cur['password']!= None:
 					flash('username already exists')
 					return redirect(url_for('signup'))
 				else:	
 					try:
-						query="INSERT INTO sellers (name, username, emailid, password) VALUES (%s, %s, %s, %s)"
-						cursor.execute(query,(name, username, emailid, password))
-						connection.commit() 
-					
+
+						# query="INSERT INTO sellers (name, username, emailid, password) VALUES (%s, %s, %s, %s)"
+						# cursor.execute(query,(name, username, emailid, password))
+						# connection.commit() 
+						query = {"type":"write", "method":"signup_seller", "emailid":emailid, "password":password, "name":name, "username":username}
+						seller_signup_ack = send_request(query)
+						print(seller_signup_ack)	
+
 					except:
 						return render_template('alert.html')
 
@@ -98,18 +115,21 @@ def login():
 		acc_type = request.form['acc_type']
 		
 		# mysql query here 
-		cursor = connection.cursor()
+		# cursor = connection.cursor()
 		if(acc_type == 'buyer'): 
-			cursor.execute("SELECT password FROM buyers WHERE username=%s", user_input)
+			# cursor.execute("SELECT password FROM buyers WHERE username=%s", user_input)
+			query = {"type":"read", "method":"login_buyer", "username":username}
 		if(acc_type == 'seller'): 
-			cursor.execute("SELECT password FROM sellers WHERE username=%s", user_input)
-		myresult =cursor.fetchone()
+			# cursor.execute("SELECT password FROM sellers WHERE username=%s", user_input)
+			query = {"type":"read", "method":"login_seller", "username":username}
+		myresult = send_request(query)	
+		
 
 
-		if myresult == None:
+		if myresult["password"] == None:
 			error = 'Invalid username or password. Please try again!'
 				
-		if myresult != None and  myresult['password']==password_input:
+		if myresult != None and  myresult["password"]==password_input:
 			session['user']=user_input
 			session['acc_type']=acc_type
 			print("acc type is ", session['acc_type'])
@@ -141,11 +161,16 @@ def dropsession():
 def cart():
 	if('user' in g):
 		#print("-------",g.userid)
-		cursor = connection.cursor()
-		query = "SELECT * FROM carts WHERE `username`=%s"
-		cursor.execute(query,g.user)
-		data = cursor.fetchall()
-		if data==None:
+
+		# cursor = connection.cursor()
+		# query = "SELECT * FROM carts WHERE `username`=%s"
+		# cursor.execute(query,g. user)
+		# data = cursor.fetchall()
+		
+		query = {"type":"read", "method":"view_cart", "username":g.user}
+		data = send_request(query)		
+
+		if len(data["product_id"]) == 0:
 			error = 'cart is empty!'
 
 		if request.method == 'POST':
@@ -153,27 +178,48 @@ def cart():
 			#print(form_obj)
 			if 'remove' in form_obj:
 				cid = form_obj['remove']
-				cursor = connection.cursor()
-				query = "DELETE FROM carts WHERE `id`=%s"
-				cursor.execute(query,cid)		
-				connection.commit()
+				# cursor = connection.cursor()
+				# query = "DELETE FROM carts WHERE `id`=%s"
+				# cursor.execute(query,cid)		
+				# connection.commit()
+				
+				query = {"type":"write", "method":"update_quantity", "product_id":cid, "new_quantity":0, "username":g.user}
+				delete_cart_ack = send_request(query)
+
 			else:
+
 				cid = form_obj['buy']
+				
+				query = {"type":"write", "method":"checkout", "product_id":cid, "username":g.user}
 
-				cursor = connection.cursor()
-				query = "SELECT * FROM `products` WHERE `id` = %s"
-				cursor.execute(query,cid)
-				results_itr  = cursor.fetchall()
-				connection.commit()
+				ret = send_request(query)
 
-				for iterator in results_itr:
-					cursor2 = connection.cursor()
-					new_stmt = g.user + " wants to buy " + iterator['product_name'] + "with id = " + str(iterator['id'])
-					query2 = "INSERT INTO `notifications` (`username`, `content`, `timestamp`) VALUES ( %s, %s, CURRENT_TIMESTAMP) " 
-					cursor2.execute(query2, (iterator['username'],new_stmt) )
-					connection.commit()
+				if(ret["error"] != ""):
+					flash('item_succesfully bought!!')
+					return redirect(url_for(cart))
+				else:
+					flash('sorry, Item out of stock')
+					return redirect(url_for(cart))
+				
+				# ret = {"ack":True, "error":""}
+				# cursor = connection.cursor()
+				# query = "SELECT * FROM `products` WHERE `id` = %s"
+				# cursor.execute(query,cid)
+				# results_itr  = cursor.fetchall()
+				# connection.commit()
+
+
+
+
+
+				############################################## update the notifications db
+				# for iterator in results_itr:
+				# 	cursor2 = connection.cursor()
+				# 	new_stmt = g.user + " wants to buy " + iterator['product_name'] + "with id = " + str(iterator['id'])
+				# 	query2 = "INSERT INTO `notifications` (`username`, `content`, `timestamp`) VALUES ( %s, %s, CURRENT_TIMESTAMP) " 
+				# 	cursor2.execute(query2, (iterator['username'],new_stmt) )
+				# 	connection.commit()
 			
-
 
 		return render_template('cart.html', data=data)
 		
@@ -187,29 +233,41 @@ def cart():
 def feed():
 	if('user' in g):
 		# print("the user is " , g.user)
-		cursor = connection.cursor()
-		query = "SELECT * FROM `products` ORDER BY `timestamp` DESC" 
-		cursor.execute(query)
-		connection.commit()
-		result = cursor.fetchall()
-
-		cursor2 = connection.cursor()
-		query2 = "SELECT * FROM `notifications` WHERE `username` = %s ORDER BY `timestamp` DESC " 
-		cursor2.execute(query2,g.user)
-		connection.commit()
-		notifications = cursor2.fetchall()
 		
+		# cursor = connection.cursor()
+		# query = "SELECT * FROM `products` ORDER BY `timestamp` DESC" 
+		# cursor.execute(query)
+		# connection.commit()
+		# result = cursor.fetchall()
+
+		query = {"type":"read", "method":"view_all_products"}
+		result = send_request(query)
+
+
+		####################################### NOTIFICATIONS
+		# cursor2 = connection.cursor()
+		# query2 = "SELECT * FROM `notifications` WHERE `username` = %s ORDER BY `timestamp` DESC " 
+		# cursor2.execute(query2,g.user)
+		# connection.commit()
+		notifications ={}
+
+
+
+
 		# search for the type of item you want to buy
 		if request.method == 'POST' and request.form['btn'] =='search':
 			form1 = request.form
 			product_type = form1['product_type']
-			if product_type == 'cab':
-				return redirect(url_for('cab'))
 
-			query = "SELECT * FROM `products` WHERE `product_type`=%s ORDER BY `timestamp` DESC" 
-			cursor.execute(query,product_type)
-			connection.commit()
-			result = cursor.fetchall()
+			# query = "SELECT * FROM `products` WHERE `product_type`=%s ORDER BY `timestamp` DESC" 
+			# cursor.execute(query,product_type)
+			# connection.commit()
+			# result = cursor.fetchall()
+			
+			query = {"type":"read", "method":"filter_products","product_type":product_type}
+			result = send_request(query)
+
+
 			return render_template('feed.html', result = result,notifications = notifications)
 
 
@@ -227,24 +285,29 @@ def feed():
 			print('product id to be bought is', product_id)
 			quantity = 1
 
-			query = "INSERT INTO `carts` (`username`,`product_id`, `quantity`) VALUES ( %s, %s, %s) "
-			cursor.execute(query,(username,product_id,quantity))
-			connection.commit()
+			# query = "INSERT INTO `carts` (`username`,`product_id`, `quantity`) VALUES ( %s, %s, %s) "
+			# cursor.execute(query,(username,product_id,quantity))
+			# connection.commit()
+
+			query = {"type":"write", "method":"add_to_cart", "product_id":product_id, "username":username, "quantity":quantity}
+			add_to_cart_ack = send_request(query)
+			print(add_to_cart_ack)
+
 
 
 			# the notification table
-			cursor = connection.cursor()
-			query = "SELECT * FROM `products` WHERE `id` = %s"
-			cursor.execute(query,request.form['btn'])
-			results_itr  = cursor.fetchall()
-			connection.commit()
+			# cursor = connection.cursor()
+			# query = "SELECT * FROM `products` WHERE `id` = %s"
+			# cursor.execute(query,request.form['btn'])
+			# results_itr  = cursor.fetchall()
+			# connection.commit()
 
-			for iterator in results_itr:
-				cursor2 = connection.cursor()
-				new_stmt = g.user + " wants to buy " + iterator['product_name'] + "with id = " + str(iterator['id'])
-				query2 = "INSERT INTO `notifications` (`username`, `content`, `timestamp`) VALUES ( %s, %s, CURRENT_TIMESTAMP) " 
-				cursor2.execute(query2, (iterator['username'],new_stmt) )
-				connection.commit()
+			# for iterator in results_itr:
+			# 	cursor2 = connection.cursor()
+			# 	new_stmt = g.user + " wants to buy " + iterator['product_name'] + "with id = " + str(iterator['id'])
+			# 	query2 = "INSERT INTO `notifications` (`username`, `content`, `timestamp`) VALUES ( %s, %s, CURRENT_TIMESTAMP) " 
+			# 	cursor2.execute(query2, (iterator['username'],new_stmt) )
+			# 	connection.commit()
 
 
 			return redirect(url_for('feed'))
@@ -268,9 +331,14 @@ def addItem():
 		price = int(ad['price'])
 		quantity = int(ad['quantity'])
 
-		query="INSERT INTO `products` (`username`, `product_type`, `product_name`, `price`, `quantity`, `timestamp`) VALUES (%s, %s, %s, %s, %s, CURRENT_TIMESTAMP)"
-		cursor.execute(query, (username, product_type, product_name, price, quantity))
-		connection.commit() 
+		# query="INSERT INTO `products` (`username`, `product_type`, `product_name`, `price`, `quantity`, `timestamp`) VALUES (%s, %s, %s, %s, %s, CURRENT_TIMESTAMP)"
+		# cursor.execute(query, (username, product_type, product_name, price, quantity))
+		# connection.commit() 
+		
+		query = {"type":"write", "method":"add_product", "username":username, "product_type":product_type, "product_name":product_name, "price":price, "quantity":quantity}
+
+		add_item_ack = send_request(query)		
+		print(add_item_ack)
 		return redirect(url_for('feed'))	
 
 	return render_template('addItem.html')
